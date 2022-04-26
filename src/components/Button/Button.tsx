218b1790a85565e61a365067bc096441f6b6f765
Aleksander <afedorenko@mail.uma.tech>
2022-04-26 07:16:18 +0300

    fix button @typescript-eslint/no-non-null-assertion

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,34 +1,43 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
-import { ButtonSt } from "./style"
+import { ButtonSt } from './style';
 
 /**
-* В компонент передаются пропсы:
-* value - строка, опредлеяющая текст кнопки
-* fullWidth - булев:
-*   true - ширина кнопки будет 100%
-*   false - ширина - auto
-* backgroundColor - строка, определяет цвет кнопки
-*   backgroundColor={"#ff0000"} - кнопка получает указанный цвет
-*   false - цвет кнопки по умолчанию
-* color - строка, аналогично backgroundColor для цвета текста
-*/
+ * В компонент передаются пропсы:
+ * value - строка, опредлеяющая текст кнопки
+ * fullWidth - булев:
+ *   true - ширина кнопки будет 100%
+ *   false - ширина - auto
+ * backgroundColor - строка, определяет цвет кнопки
+ *   backgroundColor={"#ff0000"} - кнопка получает указанный цвет
+ *   false - цвет кнопки по умолчанию
+ * color - строка, аналогично backgroundColor для цвета текста
+ */
 
 interface ButtonProps {
-    value: string;
-    fullWidth?: boolean;
-    backgroundColor?: string;
-    color?: string;
+  value: string;
+  fullWidth?: boolean;
+  backgroundColor?: string;
+  color?: string;
 }
 
-const Button: FC<ButtonProps> = ({ value, fullWidth, backgroundColor, color }) => {
-    return (<>
-        <ButtonSt
-            fullWidth={fullWidth!}
-            backgroundColor={backgroundColor!}
-            color={color!}
-        >{value}</ButtonSt>
-    </>)
-}
+const Button: FC<ButtonProps> = ({
+  value,
+  fullWidth,
+  backgroundColor,
+  color,
+}) => {
+  return (
+    <>
+      <ButtonSt
+        fullWidth={fullWidth ?? false}
+        backgroundColor={backgroundColor ?? ''}
+        color={color ?? ''}
+      >
+        {value}
+      </ButtonSt>
+    </>
+  );
+};
 
 export default Button;
