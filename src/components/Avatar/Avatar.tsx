27ee776cda6fa02a3763adcfa5c3a2130c6a766f
Aleksander <afedorenko@mail.uma.tech>
2022-04-28 21:39:01 +0300

    test

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,13 +1,52 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import { AvatarSt } from './style';
+import Input from '../Input';
 
 interface AvatarProps {
   backgroundImage: string;
 }
 
 const Avatar: FC<AvatarProps> = ({ backgroundImage }) => {
-  return <AvatarSt backgroundImage={backgroundImage} />;
+  const [avatar, setAvatar] = useState('');
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('avatar', avatar);
+
+    fetch(`https://ya-praktikum.tech/api/v2/user/profile/avatar`, {
+      method: 'PUT',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        accept: 'application/json',
+      },
+
+      body: formData,
+    }).then((data) => {
+      console.log(data);
+    });
+  };
+
+  const handleChange = (e: any) => {
+    setAvatar(e.target.files[0]);
+  };
+
+  return (
+    <AvatarSt backgroundImage={backgroundImage}>
+      <form onSubmit={onSubmit}>
+        <Input
+          name={'avatar'}
+          type={'file'}
+          placeholder={'file'}
+          onChange={handleChange}
+        />
+        <button>test!</button>
+      </form>
+    </AvatarSt>
+  );
 };
 
 export default Avatar;
