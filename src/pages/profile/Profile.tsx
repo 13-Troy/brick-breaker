8b96601e06ee64025e6799862e02700d303fd074
Aleksander <afedorenko@mail.uma.tech>
2022-04-28 06:16:26 +0300

    added logout

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Title from '../../components/Title';
 import DataLine from '../../components/DataLine';
 import Button from '../../components/Button';
 
+import { AppRoute } from '../../services/const';
+
 import { HardPopUpSt } from './style';
 
 const Profile = () => {
-  const [user, setUser] = useState({ first_name: 'tttt', id: 'yyy' });
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({ first_name: '', id: '' });
 
   useEffect(() => {
     fetch(`https://ya-praktikum.tech/api/v2/auth/user`, {
@@ -26,6 +31,20 @@ const Profile = () => {
       });
   }, []);
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch(`https://ya-praktikum.tech/api/v2/auth/logout`, {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    }).then(() => {
+      navigate(AppRoute.ROOT);
+    });
+  };
+
   return (
     <div>
       <HardPopUpSt>
@@ -33,7 +52,9 @@ const Profile = () => {
         <DataLine title={'Почта'} value={'pochta@yandex.ru'}></DataLine>
         <p>{user.first_name}</p>
         <p>{user.id}</p>
-        <Button fullWidth={false}>{'выход'}</Button>
+        <form onSubmit={onSubmit}>
+          <Button fullWidth={false}>{'выход'}</Button>
+        </form>
       </HardPopUpSt>
     </div>
   );
