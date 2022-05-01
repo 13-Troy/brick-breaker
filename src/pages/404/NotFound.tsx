362b65e31bac700c85166017150ac0da5c94e003
Aleksander <afedorenko@mail.uma.tech>
2022-05-01 14:01:56 +0300

    added 404

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../components/Button';
+
+import { CenterSt, BigSt, SmallSt } from './style';
 
 const NotFound = () => {
-  return <>404</>;
+  const navigate = useNavigate();
+
+  return (
+    <CenterSt>
+      <div>
+        <BigSt>404 </BigSt>
+        <SmallSt>упс, ошибка сервера</SmallSt>
+      </div>
+      <Button fullWidth={false} onClick={() => navigate(-1)}>
+        {'назад'}
+      </Button>
+    </CenterSt>
+  );
 };
 
 export default NotFound;
