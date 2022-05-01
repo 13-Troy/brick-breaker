826d4de3bc9217227102d16152ee3116d07f836d
Ilhom Maksadkulov <parnasmi@gmail.com>
2022-05-01 05:59:39 +0500

    refactor: move animation to Game object

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { Game } from './utils/game';
 const GAME_HEIGHT = 600;
@@ -6,30 +6,18 @@ const GAME_WIDTH = 800;
 
 const GamePage = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
-  const game = useMemo(() => new Game(GAME_WIDTH, GAME_HEIGHT), []);
-
-  const animate = useCallback(() => {
-    if (ctxRef.current) {
-      ctxRef.current.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
-      game.update();
-      game.draw(ctxRef.current);
-    }
-    requestAnimationFrame(animate);
-  }, [game]);
+  const game = useRef<Game>();
 
   useEffect(() => {
     if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d');
-
-      if (ctx === null) throw new Error('Could not get context');
-
-      ctxRef.current = ctx;
-      game.start();
-      requestAnimationFrame(animate);
+      game.current = new Game({
+        canvasElement: canvasRef.current,
+        gameWidth: GAME_WIDTH,
+        gameHeight: GAME_HEIGHT,
+      });
+      game.current?.start();
     }
-  }, [animate, game]);
+  }, []);
 
   return (
     <canvas
--- src/pages/game/utils/game.ts
+++ src/pages/game/utils/game.ts
@@ -3,24 +3,44 @@ import { InputHandler } from './input';
 import { Ball } from './ball';
 import { IBall, IPaddle } from 'pages/game/game.types';
 
+interface IGame {
+  canvasElement: HTMLCanvasElement;
+  gameWidth: number;
+  gameHeight: number;
+}
+
 export class Game {
   readonly gameWidth: number;
   readonly gameHeight: number;
   paddle: IPaddle;
   ball: IBall;
   gameObjects: [IBall, IPaddle];
-  constructor(gameWidth: number, gameHeight: number) {
+  ctx: CanvasRenderingContext2D;
+  constructor({ canvasElement, gameWidth, gameHeight }: IGame) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
-
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
-    new InputHandler(this.paddle);
     this.gameObjects = [this.ball, this.paddle];
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.ctx = canvasElement.getContext('2d')!;
+
+    new InputHandler(this.paddle);
+    this.animate = this.animate.bind(this);
   }
 
   start() {
-    console.log('started');
+    requestAnimationFrame(this.animate);
+  }
+
+  animate() {
+    if (this?.ctx) {
+      this.ctx.clearRect(0, 0, this.gameWidth, this.gameHeight);
+
+      this.update();
+      this.draw(this.ctx);
+    }
+    requestAnimationFrame(this.animate);
   }
 
   update() {
