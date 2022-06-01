92f7e59ef6e13469bbdd5e42f706c75115b19cbb
Корнеева Екатерина <ekorneeva@uma.tech>
2022-06-01 17:23:21 +0300

    add new sounds

--- client/pages/game/utils/ball.ts
+++ client/pages/game/utils/ball.ts
@@ -5,6 +5,10 @@ import { Game } from './game';
 import { Paddle } from './paddle';
 import { detectCollisionPaddle } from './detectCollisionWithPaddle';
 
+import { mediaApi } from '../../../services/media-api';
+import collect from '../../../assets/audio/collect.mp3';
+import beat from '../../../assets/audio/beat.mp3';
+
 export class Ball extends GameObject {
   readonly image: HTMLImageElement;
   public paddle: Paddle;
@@ -56,6 +60,8 @@ export class Ball extends GameObject {
   onCollateWithPaddle(paddle: GameObject) {
     console.log(`${this.name} collated with ${paddle.name}`);
     const hitbox = detectCollisionPaddle(this, paddle);
+
+    mediaApi(beat);
     /*
     detectCollisionPaddle - делить каретку на 6 почти равные части
         И возвращает порядковый номер части на который произошел столкновения с мячом
@@ -128,6 +134,7 @@ export class Ball extends GameObject {
 
   onCollateWithBrick(gameObject: GameObject) {
     console.log(`${this.name} collated with ${gameObject.name}`);
+    mediaApi(collect);
     this.game.emit('on_ball_collate');
     this.speed.y = -this.speed.y;
   }
