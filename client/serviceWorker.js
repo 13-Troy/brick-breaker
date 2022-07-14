5d705eb7cfd6a6b24cefab66aa21e11cda5c5ba0
Ilhom Maksadkulov <parnasmi@gmail.com>
2022-07-14 10:38:53 +0500

    fix service worker3

--- client/serviceWorker.js
+++ client/serviceWorker.js
@@ -1,7 +1,7 @@
 export const serviceWorker = () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('/sw.js', { scope: '/' })
+            .register('./sw.js', { scope: '/' })
             .then(() => console.log('registered'))
             .catch((error) => console.error('Service worker error', error));
     }
